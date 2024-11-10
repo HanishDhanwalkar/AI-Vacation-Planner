5346d11fc6bb7e89d67a600439619a019519c5ca
HanishDhanwalkar <hanishpd1729@gmail.com>
2024-11-10 10:14:45 +0530

    examples

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,11 +15,10 @@ st.write("Please log in to continue (username `test`, password `test`).")
 username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
-def get_username():
-    return username
+usernames= {"test": "test", "user1": "pwd"}
 
 if st.button("Log in", type="primary"):
-    if username == "test" and password == "test":
+    if username in usernames and password == usernames[username]:
         st.session_state.logged_in = True
         st.success("Logged in successfully!")
         
@@ -27,15 +26,6 @@ if st.button("Log in", type="primary"):
             f.write(username)
             
         sleep(0.5)
-        st.switch_page("pages/page1.py")
-    elif username == "user1" or password == "pwd":
-        st.session_state.logged_in = True
-        st.success("Logged in successfully!")
-        
-        with open('tmp/tmp.txt', "w") as f:
-            f.write(username)
-            
-        sleep(0.5)
-        st.switch_page("pages/page1.py")
+        st.switch_page("pages/page2.py")
     else:
         st.error("Incorrect username or password")
